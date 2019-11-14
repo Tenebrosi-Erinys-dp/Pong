96ac488b8dd6a3b6e07cdd3ff2e06af3d8935881
Tenebrosi-Erinys <nickerb40@gmail.com>
2019-11-14 13:45:58 -0800

    Implemented placeholder music, placed them in assets folder, created script to play the intro then the looped song

---
+++ Pong_Artists/Assets/Scripts/MusicController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip startingClip;
+    public AudioClip loopClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = startingClip;
+        audioSource.Play();
+        audioSource.loop = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = loopClip;
+            audioSource.Play();
+            audioSource.loop = true;
+        }
+    }
+}
